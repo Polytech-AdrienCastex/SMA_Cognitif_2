5d0f258cd505f4d90c9146ac17038609e010fb58
bvuillemin <benoit.vuil@gmail.com>
2015-11-23 17:59:28 +0100

    RENDU

--- SMA_Cognitif_2/src/model/agent/AgentNegociator.java
+++ SMA_Cognitif_2/src/model/agent/AgentNegociator.java
@@ -1,8 +1,46 @@
 package model.agent;
 
-public class AgentNegociator
-{
-    public AgentNegociator()
+public class AgentNegociator extends AgentAbstract {
+    public AgentNegociator(int priceObjective, int range, double slope)
+    {
+        super(priceObjective, range, slope);
+    }
+
+    public Builder create()
+    {
+        return new Builder();
+    }
+
+    public static class Builder extends AgentBuilder<AgentNegociator, Builder>
+    {
+        public Builder()
+        {
+        }
+
+        @Override
+        public AgentNegociator build()
+        {
+            return new AgentNegociator();
+        }
+    }
+
+    protected boolean acceptOffer(double offer)
+    {
+        return offer < priceObjective;
+    }
+
+    protected boolean rejectOffer(double offer)
+    {
+        return offer > priceObjective + range;
+    }
+
+    protected double getNextOffer(double offer, double oldOffer)
+    {
+        return (priceObjective + range - oldOffer) * slope + oldOffer;
+    }
+
+    @Override
+    public void run()
     {
 
     }
